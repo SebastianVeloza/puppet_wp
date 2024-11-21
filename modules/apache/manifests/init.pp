55a924ad53a2c32baf319ba17b9ff46d966d8f02
SebastianVeloza <fernando7829@hotmail.com>
2024-11-21 10:41:24 -0500

    configuracion de apache en la ruta de wordpress

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -8,5 +8,17 @@ class apache {
     enable => true,
   }
 
+  file { "/etc/apache2/sites-available/000-default.conf":
+    ensure  => file,
+    content => template("apache/apache.conf.erb"),
+    require => Package["apache2"],
+    notify  => Service["apache2"],
+  }
 
+  exec { "enable_mod_rewrite":
+    command => "/usr/sbin/a2enmod rewrite",
+    onlyif  => "/bin/false",
+    require => Package["apache2"],
+    notify  => Service["apache2"],
+  }
 }
