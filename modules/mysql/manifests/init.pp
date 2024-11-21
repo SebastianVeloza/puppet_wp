0a1992a620aafbb97265698f1091ddb640a0007f
SebastianVeloza <fernando7829@hotmail.com>
2024-11-21 11:23:01 -0500

    correcion bd mysql

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,26 +1,24 @@
-
 class mysql {
-  #instalacion de mysql
   package { "mysql-server":
     ensure => installed,
   }
-  #iniciamos el mysql
+
   service { "mysql":
     ensure => running,
     enable => true,
   }
 
-  #creamos la base de datos 
   exec { "create_wordpress_db":
     command => "mysql -e 'CREATE DATABASE wordpress;'",
     unless  => "mysql -e 'SHOW DATABASES;' | grep wordpress",
+    path    => ["/usr/bin", "/bin"],
     require => Package["mysql-server"],
   }
 
-  #creamos el usuario para wordpress
   exec { "create_wordpress_user":
     command => "mysql -e \"CREATE USER 'wp_user'@'localhost' IDENTIFIED BY 'wp_pass'; GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'localhost';\"",
     unless  => "mysql -e 'SELECT User FROM mysql.user;' | grep wp_user",
+    path    => ["/usr/bin", "/bin"],
     require => Exec["create_wordpress_db"],
   }
 }
